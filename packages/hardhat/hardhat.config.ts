ed8e0afa417e947a33648db68f4a3b7ffdb7f8a7
qSlnz <hub@raffenne.fr>
2021-12-04 15:04:09 +0100

    init2

---
+++ packages/hardhat/hardhat.config.ts
@@ -0,0 +1,9 @@
+// Network use on deploiement
+const defaultNetwork = "alpha";
+
+const frontEndPathConfig: { [key: string]: string } = {
+  react: "../react-app/src/utils/contracts",
+  subgraph: "../subgraph/contracts",
+};
+
+export { frontEndPathConfig };
